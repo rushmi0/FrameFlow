43ac025b32407a68fe5324756dcd194fce9c2e98
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-09-15 22:47:16 +0700

    Successfully

--- src/main/kotlin/FrameSnap.kt
+++ src/main/kotlin/FrameSnap.kt
@@ -1,49 +1,95 @@
+import java.io.BufferedReader
 import java.io.File
+import java.io.InputStreamReader
+import kotlin.concurrent.thread
 
 class FrameSnap(private val videoFileName: String, outputDirName: String) {
-
-    // ที่อยู่ของ video
     private val videoPath = "src/main/kotlin/video/$videoFileName"
-
-    // ที่อยู่ของ directory ที่จะบันทึกภาพ
     private val outputDirectory = "src/main/kotlin/$outputDirName/"
 
-    // ฟังก์ชันสำหรับแปลง video เป็น frames
-    fun snapVideo(fileExtension: String, FPS: Int): String {
-        // สร้าง directory ถ้ายังไม่มี
+    // Interface สำหรับติดตามความคืบหน้าของการประมวลผล
+    interface ProgressListener {
+        fun onProgress(progress: Int)
+    }
+
+    private var progressListener: ProgressListener? = null
+
+    // กำหนด ProgressListener
+    fun setProgressListener(listener: ProgressListener) {
+        this.progressListener = listener
+    }
+
+    // อัปเดตความคืบหน้า
+    private fun updateProgress(progress: Int) {
+        if (progressListener != null) {
+            progressListener!!.onProgress(progress)
+        }
+    }
+
+    // แสดง Progress Bar
+    private fun displayProgressBar(totalFrames: Int) {
+        var progress = 0
+        while (progress < 100) {
+            val currentFrames = countImageFiles(outputDirectory)
+            progress = ((currentFrames.toFloat() / totalFrames.toFloat() * 100).toInt()).coerceAtMost(100)
+            print("\rProgress: $progress%")
+            updateProgress(progress)  // เรียกใช้ updateProgress เพื่ออัปเดตความคืบหน้า
+            Thread.sleep(1000)
+        }
+        println("\nProcessing complete.")
+    }
+
+    // ทำการแปลงวิดีโอเป็นรูปภาพ
+    fun snapVideo(fileExtension: String, FPS: Int, CPUthreads: Int): String {
         File(outputDirectory).mkdirs()
 
-        // คำสั่ง ffmpeg สำหรับแปลง video เป็น frames
-        val ffmpegCommand = "ffmpeg -i ${this.videoPath} -vf fps=${FPS} ${this.outputDirectory}/image_%05d.$fileExtension"
+        // สร้างคำสั่ง ffmpeg สำหรับแปลงวิดีโอเป็นรูปภาพ
+        val ffmpegCommand = "ffmpeg -i ${this.videoPath} -vf fps=${FPS} -threads $CPUthreads ${this.outputDirectory}/image_%05d.$fileExtension"
 
         return try {
+            // ประมาณจำนวนเฟรมทั้งหมด
+            val totalFrames = estimateTotalFrames(FPS)
+            // เริ่มการแสดงความคืบหน้า
+            thread { displayProgressBar(totalFrames) }
+
+            // ทำการเรียกใช้คำสั่ง ffmpeg
             val process = Runtime.getRuntime().exec(ffmpegCommand)
-            val exitCode = process.waitFor()  // รอให้ ffmpeg เสร็จสิ้นการทำงาน
+            val exitCode = process.waitFor()
 
             if (exitCode == 0) {
-                val count = countImageFiles(outputDirectory)  // นับจำนวนไฟล์หลังจาก ffmpeg เสร็จสิ้น
-                "Video frames extracted successfully. \nTotal number of files $count"
+                // นับจำนวนไฟล์รูปภาพ
+                val count = countImageFiles(outputDirectory)
+                "\nVideo frames extracted successfully. \nTotal number of files $count"
             } else {
-                "Failed to extract video frames. Exit code: $exitCode"
+                // แสดงข้อความเมื่อไม่สามารถแปลงได้
+                val errorStream = BufferedReader(InputStreamReader(process.errorStream))
+                val errorMessage = errorStream.readText()
+                "Failed to extract video frames. Exit code: $exitCode\nError message:\n$errorMessage"
             }
         } catch (e: Exception) {
+            // แสดงข้อผิดพลาดที่เกิดขึ้น
             "An error occurred: ${e.message}"
         }
     }
 
-    // ฟังก์ชันนับจำนวนไฟล์ภาพ
+    // คำนวณจำนวนเฟรมทั้งหมดโดยพิจารณา FPS และความยาวของวิดีโอ
+    private fun estimateTotalFrames(FPS: Int): Int {
+        val videoDurationSeconds = 180
+        return FPS * videoDurationSeconds
+    }
+
+    // นับจำนวนไฟล์รูปภาพในโฟลเดอร์ที่กำหนด
     private fun countImageFiles(directoryPath: String): Int {
         val directory = File(directoryPath)
 
-        // ตรวจสอบว่า directory เป็นโฟลเดอร์และมีสิทธิ์เข้าถึง
-        if (directory.isDirectory) {
+        return if (directory.isDirectory) {
             val imageFiles = directory.listFiles { file ->
                 file.isFile && file.extension.toLowerCase() in listOf("jpg", "jpeg", "png", "bmp")
             }
-            return imageFiles?.size ?: 0
+            imageFiles?.size ?: 0
         } else {
             println("The specified folder was not found or does not have permission to access it.")
-            return 0
+            0
         }
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,21 @@
-
 fun main() {
+    // กำหนด ProgressListener
+    val progressListener = object : FrameSnap.ProgressListener {
+        override fun onProgress(progress: Int) {
+            print("\rProgress: $progress%")
+        }
+    }
 
-    // ตัวอย่างการใช้งาน
-    val worker1 = FrameSnap("video.mp4", "image")
-    val result = worker1.snapVideo("bmp", 2)
-    println(result)
+    // สร้าง FrameSnap instance
+    val worker1 = FrameSnap("Metallica_EnterSandman.mp4", "image1")
+    worker1.setProgressListener(progressListener)
 
+    // กำหนดนามสกุลของไฟล์, FPS, และจำนวนเธรดของ CPU
+    val fileExtension = "jpg" // * "jpg", "jpeg", "png", "bmp"
+    val fps = 2
+    val threads = 8
+
+    // เรียกใช้งาน snapVideo สำหรับการแปลงวิดีโอเป็นรูปภาพ
+    val result = worker1.snapVideo(fileExtension, fps, threads)
+    println(result)  // แสดงผลลัพธ์
 }
